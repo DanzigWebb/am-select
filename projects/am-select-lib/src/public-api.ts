433110d9137e1841ff551931686d808ebd9e5892
Alexander <valverin95@gmail.com>
2022-01-27 18:49:56 +0300

    init lib

---
+++ projects/am-select-lib/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of am-select-lib
+ */
+
+export * from './lib/am-select.module';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AmSelectModule } from '../../projects/am-select-lib/src/lib/am-select.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AmSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
